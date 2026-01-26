d42578fbb8fd52017c2a77eb44f7eb4fb4aad5ca
Tharindu Balasooriya <tharindutsb@gmail.com>
2026-01-27 01:00:03 +0530

    dark mode setup

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,10 +1,39 @@
 import { Moon,Sun } from "lucide-react"; 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  return <button>{isDarkMode ? <Sun /> : <Moon />}</button>;
+   useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme=="dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    }else {
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    }
+  }, []);
 
+  const tooggleTheme = () => {  
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+       localStorage.setItem("theme", "light");
+      setIsDarkMode(false); 
+  }else { 
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    setIsDarkMode(true); } 
+  };
+
+  return (<button onClick={tooggleTheme}>
+    {""}
+    {isDarkMode ? (
+      <Sun className="h-6 w-6 text-yellow-300" />
+    ) : (
+    <Moon className="h-6 w-6 text-blue-900"/>
+    )}
+    </button>
+    );
 }
